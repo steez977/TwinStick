1cae2dc23945ea4022ffd4ee73f55e2791e9221b
Stijn <stijnvervaet@gmail.com>
2018-09-29 17:08:21 +0100

    Playing around with Git

---
+++ Assets/TestGit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       Debug.Log("Testing Github"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
